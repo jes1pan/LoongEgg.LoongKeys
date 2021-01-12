ed2de6eba892c7808e82c4b7f9f330efe5e6c0be
jeansx mas <1595126220@qq.com>
2021-01-12 08:57:19 +0800

    create

---
+++ LoongEgg.ViewModelBase/RelayCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace LoongEgg.ViewModelBase
+{
+    public class RelayCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged = (s, e) => { };
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _Execute = execute;
+            _CanExcute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+            => _CanExcute == null ? true : _CanExcute(parameter);
+        private readonly Func<object, bool> _CanExcute = null;
+
+        public void Execute(object parameter)
+            => _Execute(parameter);
+        private readonly Action<object> _Execute;
+    }
+}
---
+++ LoongEgg.ViewModelBase/RelayCommand{T}.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace LoongEgg.ViewModelBase
+{
+    public class RelayCommand<T> :ICommand
+    {
+        public event EventHandler CanExecuteChanged = (s, e) => { };
+
+        public RelayCommand(Action<T> execute, Func<T, bool> canExecute = null)
+        {
+            _Execute = execute;
+            _CanExcute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+            => _CanExcute == null ? true : _CanExcute((T)parameter);
+        private readonly Func<T, bool> _CanExcute = null;
+
+        public void Execute(object parameter)
+            => _Execute((T)parameter);
+        private readonly Action<T> _Execute;
+    }
+}
