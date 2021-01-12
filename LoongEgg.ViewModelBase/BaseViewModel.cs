d39eb465b6d45167d6c675fd89d939e370b97cc3
jeansx mas <1595126220@qq.com>
2021-01-12 08:57:19 +0800

    标题拖动隐藏透明update1

---
+++ LoongEgg.ViewModelBase/BaseViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoongEgg.ViewModelBase
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// 通知前端属性改变事件
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        protected virtual void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+
+        public bool Set<T>(ref T target, T value, [CallerMemberName] string propertyName = null)
+        {
+            //value没有发生变化
+            if (EqualityComparer<T>.Default.Equals(target, value))
+                return false;
+
+            
+            target = value;
+            RaisePropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
---
+++ LoongEgg.ViewModelBase/WindowStateManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace LoongEgg.ViewModelBase
+{
+    public class WindowStateManager : _WindowStateManger
+    {
+        /// <summary>
+        ///     负责控制Windows的最大/最小化/还原和关闭窗口
+        ///     NOTE: 使用语法
+        ///         1.设置父级DataContext="{x:static DemoWindowStateManager.Instance}"
+        ///         2.Button的Command="{Binding CommandClose}", CommandParameter="{Binding ElementName=thisWindow}"
+        ///         3.thisWindow为要关闭的窗体名字
+        ///         4.或者 CommandParameter="{Binding RelativeSource={RelativeSource AncestorType=Window}}"
+        /// </summary>
+        public static WindowStateManager Instance => _Instance ?? (_Instance = new WindowStateManager());
+        private static WindowStateManager _Instance = null;
+        public WindowStateManager()
+        {
+            CommandMinimze = new RelayCommand<Window>((win) => win.WindowState = WindowState.Minimized);
+            CommandMaximize = new RelayCommand<Window>((win) => win.WindowState ^= WindowState.Maximized);
+            CommandClose = new RelayCommand<Window>((win) => win.Close());
+        }
+    }
+
+    public abstract class _WindowStateManger
+    {
+        public ICommand CommandMinimze { get; protected set; }
+        public ICommand CommandMaximize { get; protected set; }
+        public ICommand CommandClose { get; protected set; }
+    }
+}
