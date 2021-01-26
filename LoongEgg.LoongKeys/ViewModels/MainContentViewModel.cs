fe78edfc2ec60111646283a8f4f1420f5e7cdd08
jeansx mas <1595126220@qq.com>
2021-01-26 16:16:47 +0800

    ButtonStyles

---
+++ LoongEgg.LoongKeys/ViewModels/MainContentViewModel.cs
@@ -0,0 +1,25 @@
+﻿using LoongEgg.ViewModelBase;
+using System.Windows.Input;
+
+namespace LoongEgg.LoongKeys
+{
+    class MainContentViewModel : BaseViewModel
+    {
+        /// <summary>
+        /// 按键可用标志
+        /// </summary>
+        public bool ButtonIsEnabled
+        {
+            get => _ButtonIsEnabled;
+            set => Set(ref _ButtonIsEnabled, value);
+        }
+        private bool _ButtonIsEnabled;
+
+        /// <summary>
+        /// 翻转<see cref="ButtonIsEnabled"/>命令
+        /// </summary>
+        private RelayCommand _CommandFlipButtonIsEnable;
+        public ICommand CommandFlipButtonIsEnable =>
+            _CommandFlipButtonIsEnable ?? (_CommandFlipButtonIsEnable = new RelayCommand((e) => ButtonIsEnabled = !ButtonIsEnabled));
+    }
+}
